44bc9622fcb92664a125329d12373c479f705550
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-04 13:06:10 +0100

    Adding cohort to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804115232) do
+ActiveRecord::Schema.define(version: 20150804115904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150804115232) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.text     "username"
+    t.text     "cohort"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
