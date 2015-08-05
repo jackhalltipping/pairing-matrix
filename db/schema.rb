e26f246efe37571036020e46bdba693e25cc19b2
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-05 11:30:22 +0100

    Creating cohort model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804115904) do
+ActiveRecord::Schema.define(version: 20150805102233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,7 +37,6 @@ ActiveRecord::Schema.define(version: 20150804115904) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.text     "username"
-    t.text     "cohort"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
