f1cada6b58b6ce883456f8b2dc5a88a18dc6e31e
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-05 15:17:42 +0100

    Creating pairing table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805104205) do
+ActiveRecord::Schema.define(version: 20150805141146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150805104205) do
     t.date     "start_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pairings", force: :cascade do |t|
+    t.date     "day"
+    t.integer  "user_id"
+    t.integer  "pair_id"
+    t.boolean  "paired_with", default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "users", force: :cascade do |t|
