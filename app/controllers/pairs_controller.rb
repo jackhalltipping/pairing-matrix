78a5e3aa98602a93c7a271cc7d51e1f4f272072c
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-06 12:49:17 +0100

    Getting today, tomorrow and week's pair for current user from pairing table

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -4,6 +4,19 @@ class PairsController < ApplicationController
   end
 
   def show
+    id = current_user.id
+    @user = User.find(id)
+    today = Date.today
+    tomorrow = Date.tomorrow
+    days_of_the_week = (today.at_beginning_of_week..today.at_end_of_week).to_a[0..4]
+    @today_pair = get_pair(id, today)
+    @tomorrow_pair = get_pair(id, tomorrow)
+    @weeks_pairs = days_of_the_week.map { |day| get_pair(id, day) }
+  end
+
+  def get_pair id,date
+    pair_id = Pairing.where(day: date, user_id: id)[0].pair_id
+    pair = User.find(pair_id)
   end
 
 end
