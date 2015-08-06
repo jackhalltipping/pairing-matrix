3879b667ca2d8412e30a70db37224b6e7d18cc78
KateWilkinson <kate.wilkinson@hotmail.com>
2015-08-06 13:23:58 +0100

    correct pairs logic added to show view; added js file but not yet working

---
+++ app/assets/javascripts/pairs.js
@@ -0,0 +1,21 @@
+// $(document).ready(function () {
+//             $('#today').hide()
+//             $('#tomorrow').hide()
+//             $('#this-week').hide()
+// });
+
+// // $('#today-btn').click(function () {
+// //   console.log('hello')
+// //   // $('#tomorrow').hide()
+// //   // $('#this-week').hide()
+// // });
+
+// // $('#tomorrow-btn').click(function (event) {
+// //   $('#tomorrow').show()
+// //   event.preventDefault();
+// // });
+
+// // $('#week-btn').click(function (event) {
+// //   $('#this-week').show()
+// //   event.preventDefault();
+// // });
