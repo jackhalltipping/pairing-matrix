e5545bdb214a153143201d5d48527c6346d47712
KateWilkinson <kate.wilkinson@hotmail.com>
2015-08-07 12:44:43 +0100

    github avatars

--- app/assets/javascripts/pairs.js
+++ app/assets/javascripts/pairs.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
             $('#tomorrow').hide();
             $('#this-week').hide();
 
+  getUserAvatar($('#hidden-user').html(), "github-avatar" );
+  getUserAvatar($('#hidden-pair').html(), "pair-avatar" );
+
 
   $('#today-btn').click(function () {
     $('#today').show();
@@ -21,3 +24,12 @@ $(document).ready(function () {
     $('#tomorrow').hide();
   });
 });
+
+function getUserAvatar(github, imageclass){
+  var url = 'https://api.github.com/users/' + github ;
+  var response = $.get(url).done(function() {
+    var avatar = response.responseJSON.avatar_url
+    $("." + imageclass).prepend('<img src='+avatar+'&s=200 />');
+  });
+};
+
