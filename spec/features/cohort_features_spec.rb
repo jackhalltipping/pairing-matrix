6f9efa51e32ecd62525d9590b4fe192071d338c5
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-07 11:52:31 +0100

    Passing tests after routing to show page on login

--- spec/features/cohort_features_spec.rb
+++ spec/features/cohort_features_spec.rb
@@ -5,13 +5,25 @@ feature "Viewing cohorts" do
   let!(:cohort_with_users) { create(:cohort_with_users) }
   let!(:user) { cohort_with_users.users.first }
   let!(:another_user) { cohort_with_users.users.last }
-  before { login_as(user, :scope => :user) }
+  before do
+    create_pairing_for_the_week(user)
+    login_as(user, :scope => :user)
+  end
 
   scenario "when logged in a user can see a list of everyone in her cohort" do
     visit "/"
     click_button "My Cohort"
     expect(page).to have_content "#{another_user.username}"
     expect(page).to have_content "#{user.username}"
+  end
+
+  def create_pairing_for_the_week user
+    today = Date.today
+    days_of_the_week = (today.at_beginning_of_week..today.at_end_of_week).to_a[0..4]
+    days_of_the_week.each do |day|
+      another_user = create(:user)
+      Pairing.create(day: day, user_id: user.id, pair_id: another_user.id)
+    end
   end
 
 end
--- spec/features/pairing_feature_spec.rb
+++ spec/features/pairing_feature_spec.rb
@@ -7,15 +7,25 @@ feature 'Pairing' do
   context 'user is not logged in' do
     scenario 'user cannot see pairing buttons' do
       visit '/'
-      expect(page).not_to have_content('See my pairs')
+      expect(current_url).not_to have_content('/pairs/show')
     end
   end
 
   context 'user is logged in' do
+
+    before do
+      today = Date.today
+      days_of_the_week = (today.at_beginning_of_week..today.at_end_of_week).to_a[0..4]
+      days_of_the_week.each do |day|
+        another_user = create(:user)
+        Pairing.create(day: day, user_id: user.id, pair_id: another_user.id)
+      end
+    end
+
     scenario 'user can see pairing buttons' do
       login_as(user, :scope => :user)
       visit '/'
-      expect(page).to have_content('See my pairs')
+      expect(current_url).to have_content('/pairs/show')
     end
   end
 
